a4125adadabf056c3d18ba249f1a135401c328a0
Gimano <112472723+Gimano@users.noreply.github.com>
2022-10-17 14:23:18 +0200

    Add project files.

---
+++ Statistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beskrivande_Statistik
+{
+    public static class Statistics
+    {
+        public static dynamic DescriptiveStatistics(int[] i)
+        {
+            return i;
+        }
+
+        public static int Maximum(int[] i)
+        {
+            return i[0];
+        }
+
+        public static double Mean(int[] i)
+        {
+            return (double)i[0];
+        }
+
+        public static double Median(int[] i)
+        {
+            return Math.Sqrt(i[0]);
+        }
+
+        public static int Minimum(int[] i)
+        {
+            return i[0];
+        }
+
+        public static int[] Mode(int[] i)
+        {
+            return i;
+        }
+
+        public static int Range(int[] i)
+        {
+            return i[0];
+        }
+        public static double StandardDeviation(int[] i)
+        {
+            return (double)(i[0]);
+        }
+    }
+}
