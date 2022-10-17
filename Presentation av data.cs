111c20362e172a3398194345547bbf8f42c65dfc
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-17 15:23:57 +0200

    1st draft

--- Presentation av data.cs
+++ Presentation av data.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace Beskrivande_Statistik
 {
     internal class Presentation_av_data    //Vill testa merge/ pull / push till Git 
     {
-        
+        public static void presentation_av_data()    //vill ha olika bakgrundfärg då bli texten lättläst
+        {
+            Console.WriteLine($"Maximun:  " + 
+                $"\nMinimum: " +
+                $"\nMedelvärde: " +
+                $"\nMedian: " +
+                $"\nTypvärde: " +
+                $"\nVaiationsbredd: " +
+                $"\nStandardavvikelse: ");
+        }
     }
 }
