df5223838e15d013a6a0c93a10640115cee72434
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-17 15:12:59 +0200

    skapar class presentation av data

---
+++ Presentation av data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beskrivande_Statistik
+{
+    internal class Presentation_av_data    //Vill testa merge/ pull / push till Git 
+    {
+        
+    }
+}
