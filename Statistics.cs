7b1ddfa41dc741ad057c491368d809e2636ae931
Herman Löfberg <herman.lofberg@telia.com>
2022-10-18 10:40:27 +0200

    standarddevation som kanske funkar

--- Statistics.cs
+++ Statistics.cs
@@ -15,11 +15,19 @@ namespace Beskrivande_Statistik
 
         public static int Maximum(int[] source)
         {
+            int maximum = 0;
+            maximum = source.Max();
+            return maximum;
+
             return source[0];
         }
 
         public static double Mean(int[] source)
         {
+            double mean = 0;
+            mean = source.Average();
+            return Math.Round(mean, 1);
+
             return (double)source[0];
         }
 
@@ -30,11 +38,17 @@ namespace Beskrivande_Statistik
 
         public static int Minimum(int[] source)
         {
+            int minimum = 0;
+            minimum = source.Min();
+            return minimum;
+
             return source[0];
         }
 
         public static int[] Mode(int[] source)
         {
+            return source;
+
             if (source == null)
                 throw new ArgumentNullException("Sequence is null.");
             if (source.Length == 0)
@@ -52,11 +66,33 @@ namespace Beskrivande_Statistik
 
         public static int Range(int[] source)
         {
+            int range = 0;
+            range = source.Max() - source.Min();
+            return range;
+
             return source[0];
         }
         public static double StandardDeviation(int[] source)
         {
-            return (double)(source[0]);
+            double devation = 0;
+            int count = source.Count();
+            double medel = Mean(source);
+
+            if (count > 1)
+            {
+                double sum = source.Sum(d=> (d-medel) * (d-medel));
+                
+                devation = Math.Sqrt(sum / count);
+            }
+            return devation;
+
+            //varje number i listan - medelvärde för att få avvikelse för varje nummer
+            //kvadera varje avvikelsen för varje tal
+
+            //plus ihop alla kvaderade avvikelser och dela det på antalet nummer
+            //sedan roten ur på svaret och efter det har du standardavvikelsen
+
+           
         }
     }
 }
