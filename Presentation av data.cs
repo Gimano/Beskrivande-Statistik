3ccf0d4da2dc1ec9365d909052a2d8612713ce69
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-18 10:34:50 +0200

    fix stavning

--- Presentation av data.cs
+++ Presentation av data.cs
@@ -9,8 +9,9 @@ namespace Beskrivande_Statistik
 {
     internal class Presentation_av_data    //Vill testa merge/ pull / push till Git 
     {
-        public static void presentation_av_data()    //vill ha olika bakgrundfärg då bli texten lättläst
+        public static void presentation_av_data()    //vill ha olika färg då bli texten lättläst
         {
+            Console.ForegroundColor = ConsoleColor.White;   
             Console.WriteLine($"Maximun:  ");                   //vit
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine($"\nMinimum: ");                  //grå
@@ -21,7 +22,7 @@ namespace Beskrivande_Statistik
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine($"\nTypvärde: ");                 //vit 
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine($"\nVaiationsbredd: ");           //grå
+            Console.WriteLine($"\nVariationsbredd: ");           //grå
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine($"\nStandardavvikelse: ");        //vit
             Console.ForegroundColor = ConsoleColor.Gray;        //grå 
