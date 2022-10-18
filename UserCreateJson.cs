eac1a2043f76c8c415921cec1250697d089e03b1
Pingeln <niklasmartinsson89@gmail.com>
2022-10-18 11:38:48 +0200

    börjat med en klass med metod för att skapa egen json fil och skicka in egna värden från användaren

---
+++ UserCreateJson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beskrivande_Statistik
+{
+    public static class UserCreateJson
+    {
+        public static int[] UserJson()
+        {
+            Console.Write("Name of your list: ");
+            string fileName = Console.ReadLine();
+            List<int> IntList = new List<int>();
+            Console.Write("Input integer number to your json file (ESCAPE TO EXIT): ");
+            var input = Console.ReadKey();
+            do
+            {
+                IntList.Add()
+            } while (input.Key != ConsoleKey.Escape);
+
+            // placeholder return to keep syntax errors away while coding
+            return null;
+        }
+    }
+}
