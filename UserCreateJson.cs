e8865b370c2cc82b9895f022dfce20c5b780fc54
Pingeln <niklasmartinsson89@gmail.com>
2022-10-18 14:37:09 +0200

    Nu funkar det att skapa en egen .JSON fil in i debug/net6.0 mappen och den tar in alla värden från listan

--- UserCreateJson.cs
+++ UserCreateJson.cs
@@ -13,10 +13,10 @@ namespace Beskrivande_Statistik
         {
             Console.Write("Name of your list: ");
             string fileName = Console.ReadLine();
-            
+            fileName = fileName + ".json";
             List<int> IntList = new List<int>();
             Console.WriteLine("Input integer numbers to your json file, type 0 to finish.");
-            StreamWriter file = File.CreateText($"{fileName}.json");
+            //StreamWriter file = File.CreateText($"{fileName}.json");
             // Let user add integers to IntList untill input is 0, also if formatexception go to label and keep looping
             InvalidInput:
             while (true)
@@ -24,16 +24,16 @@ namespace Beskrivande_Statistik
                 try
                 {
                     Console.Write("Number to add: ");
-                    IntList.Add(Convert.ToInt32(Console.ReadLine()));
-                    if (IntList.Last() == 0)
+                    int input = int.Parse(Console.ReadLine());
+
+                    if (input == 0)
                     {
-                        foreach (int i in IntList)
-                        {
-                            
-                        }
-                        
+                        string json = JsonConvert.SerializeObject(IntList, Formatting.Indented);
+                        File.WriteAllText(fileName, json);
                         break;
                     }
+                    else
+                        IntList.Add(input);
                 }
                 catch(FormatException)
                 {
