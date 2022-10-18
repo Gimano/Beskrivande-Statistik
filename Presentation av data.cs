3eece7bd9abd0f51a0ef24a41cf5eafcb19cdfdc
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-18 10:24:30 +0200

    ändra färg

--- Presentation av data.cs
+++ Presentation av data.cs
@@ -11,13 +11,21 @@ namespace Beskrivande_Statistik
     {
         public static void presentation_av_data()    //vill ha olika bakgrundfärg då bli texten lättläst
         {
-            Console.WriteLine($"Maximun:  " +
-                $"\n\nMinimum: " +
-                $"\n\nMedelvärde: " +
-                $"\n\nMedian: " +
-                $"\n\nTypvärde: " +
-                $"\n\nVaiationsbredd: " +
-                $"\n\nStandardavvikelse: ");
+            Console.WriteLine($"Maximun:  ");                   //vit
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"\nMinimum: ");                  //grå
+            Console.ForegroundColor = ConsoleColor.White;       
+            Console.WriteLine($"\nMedelvärde: ");               //vit
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"\nMedian: ");                   //grå
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\nTypvärde: ");                 //vit 
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"\nVaiationsbredd: ");           //grå
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\nStandardavvikelse: ");        //vit
+            Console.ForegroundColor = ConsoleColor.Gray;        //grå 
+
         }
     }
 }
