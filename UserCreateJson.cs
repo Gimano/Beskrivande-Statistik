7e0c4f594182a8cdc39dd8ef051c462a259f597c
Pingeln <niklasmartinsson89@gmail.com>
2022-10-23 18:37:07 +0200

    User can now create a json with randomly generated integers - they chose how many, and within which range.

--- UserCreateJson.cs
+++ UserCreateJson.cs
@@ -17,39 +17,65 @@ namespace Beskrivande_Statistik
             string fileName = Console.ReadLine();
             fileName = fileName + ".json";
             List<int> IntList = new List<int>();
-            Console.WriteLine("Input integer numbers to your json file, type 0 to finish.");
-            // Let user add integers to IntList untill input is 0, also if formatexception go to label and keep looping
-            InvalidInput:
-            while (true)
-            {
-                try
-                {
-                    Console.Write("Number to add: ");
-                    int input = int.Parse(Console.ReadLine());
+            Console.Clear();
+        InvalidInput:
 
-                    if (input != 0)
+            Console.Write($"[1] Manually add integers to {fileName}.\n[2] Fill {fileName} with random integers.\nInput: ");
+            string jsonChoice = Console.ReadLine();
+            switch (jsonChoice)
+            {
+                case "1":
+                    // Let user add integers to IntList untill input is 0, also if formatexception go to label and keep looping
+                    while (true)
                     {
-                        IntList.Add(input);
+                        try
+                        {
+                            Console.WriteLine("Input integer numbers to your json file, type 0 to finish.");
+                            Console.Write("Number to add: ");
+                            int input = int.Parse(Console.ReadLine());
+
+                            if (input != 0)
+                            {
+                                IntList.Add(input);
+                            }
+                            else
+                            {
+                                // serializes the elements of IntList and writes them to fileName.json, with indentation
+                                string json = JsonConvert.SerializeObject(IntList, Formatting.Indented);
+                                File.WriteAllText(fileName, json);
+                                break;
+                            }
+                        }
+                        // Catches possible exceptions and sends the user back to keep adding integers untill user enters 0 to exit.
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Invalid input. Input only accept integers.");
+                            goto InvalidInput;
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Input out of range, stay inside integer range please.");
+                            goto InvalidInput;
+                        }
                     }
-                    else
+                    break;
+
+                    case "2":
+                    Console.Write("How many randomly generated integer numbers do you want to add to your list?: ");
+                    int numberOfInts = int.Parse(Console.ReadLine());
+                    Console.Write("Lowest possible number to be generated: ");
+                    int lowRnd = int.Parse(Console.ReadLine());
+                    Console.Write("Highest possible number to be generated: ");
+                    int highRnd = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < numberOfInts; i++)
                     {
-                        // serializes the elements of IntList and writes them to fileName.json, with indentation
-                        string json = JsonConvert.SerializeObject(IntList, Formatting.Indented);
-                        File.WriteAllText(fileName, json);
-                        break;
+                        Random rnd = new Random();
+                        int num = rnd.Next(lowRnd, highRnd);
+                        IntList.Add(num);
                     }
-                }
-                // Catches possible exceptions and sends the user back to keep adding integers untill user enters 0 to exit.
-                catch(FormatException)
-                {
-                    Console.WriteLine("Invalid input. Input only accept integers.");
-                    goto InvalidInput;
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("Input out of range, stay inside integer range please.");
-                    goto InvalidInput;
-                }
+                    string jsonWithRnd = JsonConvert.SerializeObject(IntList, Formatting.Indented);
+                    File.WriteAllText(fileName, jsonWithRnd);
+                    break;
             }
         }
     }
