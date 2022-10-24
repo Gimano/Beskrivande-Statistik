80a5fdf8040f691aa5243d44d66b5ffde4e173f6
Pingeln <niklasmartinsson89@gmail.com>
2022-10-24 15:28:12 +0200

    numberofInts range 1 - 10.000.000 med varning över 1.000.000

--- UserCreateJson.cs
+++ UserCreateJson.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,35 +31,57 @@ namespace Beskrivande_Statistik
                     while (true)
                     {
                         int input = 0;
-                            Console.WriteLine("Input integer numbers to your json file, type 0 to finish.");
-                            Console.Write("Number to add: ");
+                        Console.WriteLine("Input integer numbers to your json file, type 0 to finish.");
+                        Console.Write("Number to add: ");
                         input = ExceptionHandling(input);
 
-                            if (input != 0)
-                            {
-                                IntList.Add(input);
-                            }
-                            else
-                            {
-                                // serializes the elements of IntList and writes them to fileName.json, with indentation
-                                string json = JsonConvert.SerializeObject(IntList, Formatting.Indented);
-                                File.WriteAllText(fileName, json);
-                                break;
-                            }
+                        if (input != 0)
+                        {
+                            IntList.Add(input);
+                        }
+                        else
+                        {
+                            // serializes the elements of IntList and writes them to fileName.json, with indentation
+                            string json = JsonConvert.SerializeObject(IntList, Formatting.Indented);
+                            File.WriteAllText(fileName, json);
+                            break;
+                        }
                     }
                     break;
 
-                    // OM LÄGSTA ÄR STÖRRE ÄN HÖGSTA BLIR DET ERROR. FIXA DET MED TEMP VARIABLER
-                    // SÄTT EN RANGE PÅ 1 - 10 MILJONER PÅ numberOfInts
-                    // Om man väljer numberOfInts över 1 miljon ge en varning - Vill du verkligen göra det här?
+                // OM LÄGSTA ÄR STÖRRE ÄN HÖGSTA BLIR DET ERROR. FIXA DET MED TEMP VARIABLER
+                // Om man väljer numberOfInts över 1 miljon ge en varning - Vill du verkligen göra det här?
 
                 // lets the user chose how many integers to be added to the jsonfile and also chose within which range the numbers can be created from a random.
                 case "2":
                     int numberOfInts = 0;
                     int lowRnd = 0;
                     int highRnd = 0;
+                    string confirmInts = "";
+                regretDecision:
                     Console.Write("How many randomly generated integer numbers do you want to add to your list?: ");
                     numberOfInts = ExceptionHandling(numberOfInts);
+                    while (numberOfInts < 1 || numberOfInts > 10000000)
+                    {
+                        Console.WriteLine("Range has to be between 1 - 10.000.000. Please try again.");
+                        numberOfInts = ExceptionHandling(numberOfInts);
+                    }
+                    if (numberOfInts >= 1000000)
+                    {
+                        Console.Write("You chose to generate more than 1 million integers into a file.\nThis will take up alot of resources and space from your computer.\nAre you sure you want to continue? (Y/N): ");
+                        
+                        while (confirmInts != "N")
+                        {
+                            confirmInts = Console.ReadLine().ToUpper();
+                            if (confirmInts == "Y")
+                            {
+                                Console.WriteLine($"Ok. Generating file with {numberOfInts} integers. We did warn you.");
+                                break;
+                            }
+                            else if (confirmInts == "N") goto regretDecision;
+                            else Console.Write("Invalid option. Do you want to continue? (Y/N): ");
+                        }
+                    }
                     Console.Write("Lowest possible number to be generated: ");
                     lowRnd = ExceptionHandling(lowRnd);
                     Console.Write("Highest possible number to be generated: ");
@@ -80,7 +103,7 @@ namespace Beskrivande_Statistik
         // I made this to avoid repeating myself with alot of try catches in the rest of the code.
         public static int ExceptionHandling(int intToTry)
         {
-            ExceptionHandlingLabel:
+        ExceptionHandlingLabel:
             try
             {
                 int temp = int.Parse(Console.ReadLine());
