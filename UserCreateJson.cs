4d893a7b4eeeb0f66e28ab6b270f83bc8ef39fd0
Pingeln <niklasmartinsson89@gmail.com>
2022-10-25 13:46:23 +0200

    Buggfix i menyn där felaktig inmatning tog oss direkt till main menyn istället för att ta in ett nytt input.

--- UserCreateJson.cs
+++ UserCreateJson.cs
@@ -20,91 +20,97 @@ namespace Beskrivande_Statistik
             fileName = fileName + ".json";
             List<int> IntList = new List<int>();
             Console.Clear();
+            string jsonChoice = "";
         InvalidInput:
-
-            Console.Write($"[1] Manually add integers to {fileName}.\n[2] Fill {fileName} with random integers.\nInput: ");
-            string jsonChoice = Console.ReadLine();         
-            switch (jsonChoice)
+            while (jsonChoice != "0") 
             {
-                case "1":
-                    // Let user add integers to IntList untill input is 0.
-                    while (true)
-                    {
-                        int input = 0;
-                        Console.WriteLine("Input integer numbers to your json file, type 0 to finish.");
-                        Console.Write("Number to add: ");
-                        input = ExceptionHandling(input);
-
-                        if (input != 0)
+                Console.Write($"[1] Manually add integers to {fileName}.\n[2] Fill {fileName} with random integers.\n[0] Exit to menu\nInput: ");
+                jsonChoice = Console.ReadLine();
+                switch (jsonChoice)
+                {
+                    case "1":
+                        // Let user add integers to IntList untill input is 0.
+                        while (true)
                         {
-                            IntList.Add(input);
-                        }
-                        else
-                        {
-                            // serializes the elements of IntList and writes them to fileName.json, with indentation
-                            string json = JsonConvert.SerializeObject(IntList, Formatting.Indented);
-                            File.WriteAllText(fileName, json);
-                            break;
-                        }
-                    }
-                    break;
+                            int input = 0;
+                            Console.WriteLine("Input integer numbers to your json file, type 0 to finish.");
+                            Console.Write("Number to add: ");
+                            input = ExceptionHandling(input);
 
-                // OM LÄGSTA ÄR STÖRRE ÄN HÖGSTA BLIR DET ERROR. FIXA DET MED TEMP VARIABLER
-
-                // lets the user chose how many integers to be added to the jsonfile and also chose within which range the numbers can be created from a random. (1 - 10 million capped)
-                // if the user choses more than 1 million - ask them if they want to proceed
-                case "2":
-                    int numberOfInts = 0;
-                    int lowRnd = 0;
-                    int highRnd = 0;                
-                    string confirmInts = "";
-
-                regretDecision:
-                    Console.Write("How many randomly generated integer numbers do you want to add to your list?: ");
-                    numberOfInts = ExceptionHandling(numberOfInts);
-                   
-                    while (numberOfInts < 1 || numberOfInts > 10000000)
-                    {
-                        Console.WriteLine("Range has to be between 1 - 10.000.000. Please try again.");
-                        numberOfInts = ExceptionHandling(numberOfInts);
-                    }
-                   
-                    if (numberOfInts >= 1000000)
-                    {
-                        Console.Write("You chose to generate more than 1 million integers into a file.\nThis will take up alot of resources and space from your computer.\nAre you sure you want to continue? (Y/N): ");
-                        
-                        while (confirmInts != "N")
-                        {
-                            confirmInts = Console.ReadLine().ToUpper();
-                            if (confirmInts == "Y")
+                            if (input != 0)
                             {
-                                Console.WriteLine($"Ok. Generating file with {numberOfInts} integers. We did warn you.");
+                                IntList.Add(input);
+                            }
+                            else
+                            {
+                                // serializes the elements of IntList and writes them to fileName.json, with indentation
+                                string json = JsonConvert.SerializeObject(IntList, Formatting.Indented);
+                                File.WriteAllText(fileName, json);
                                 break;
                             }
-                            else if (confirmInts == "N") goto regretDecision;
-                            else Console.Write("Invalid option. Do you want to continue? (Y/N): ");
                         }
-                    }
-                   
-                    Console.Write("Lowest possible number to be generated: ");
-                    lowRnd = ExceptionHandling(lowRnd);
-                    
-                    while (lowRnd >= highRnd)
-                    { 
-                        Console.Write($"Highest possible number to be generated (can not be lower than {lowRnd}): ");
-                        highRnd = ExceptionHandling(highRnd);
-                    }
-                    
-                    for (int i = 0; i < numberOfInts; i++)
-                    {
-                        Random rnd = new Random();
-                        int num = rnd.Next(lowRnd, highRnd);
-                        IntList.Add(num);
-                    }
-                    // serializes the elements of IntList and writes them to fileName.json, with indentation
-                    string jsonWithRnd = JsonConvert.SerializeObject(IntList, Formatting.Indented);
-                    File.WriteAllText(fileName, jsonWithRnd);
-                    break;
+                        break;
+
+                    // lets the user chose how many integers to be added to the jsonfile and also chose within which range the numbers can be created from a random. (1 - 10 million capped)
+                    // if the user choses more than 1 million - ask them if they want to proceed
+                    case "2":
+                        int numberOfInts = 0;
+                        int lowRnd = 0;
+                        int highRnd = 0;
+                        string confirmInts = "";
+
+                    regretDecision:
+                        Console.Write("How many randomly generated integer numbers do you want to add to your list?: ");
+                        numberOfInts = ExceptionHandling(numberOfInts);
+
+                        while (numberOfInts < 1 || numberOfInts > 10000000)
+                        {
+                            Console.WriteLine("Range has to be between 1 - 10.000.000. Please try again.");
+                            numberOfInts = ExceptionHandling(numberOfInts);
+                        }
+
+                        if (numberOfInts >= 1000000)
+                        {
+                            Console.Write("You chose to generate more than 1 million integers into a file.\nThis will take up alot of resources and space from your computer.\nAre you sure you want to continue? (Y/N): ");
+
+                            while (confirmInts != "N")
+                            {
+                                confirmInts = Console.ReadLine().ToUpper();
+                                if (confirmInts == "Y")
+                                {
+                                    Console.WriteLine($"Ok. Generating file with {numberOfInts} integers. We did warn you.");
+                                    break;
+                                }
+                                else if (confirmInts == "N") goto regretDecision;
+                                else Console.Write("Invalid option. Do you want to continue? (Y/N): ");
+                            }
+                        }
+
+                        Console.Write("Lowest possible number to be generated: ");
+                        lowRnd = ExceptionHandling(lowRnd);
+
+                        do
+                        {
+                            Console.Write($"Highest possible number to be generated (can not be lower than {lowRnd}): ");
+                            highRnd = ExceptionHandling(highRnd);
+                        } while (lowRnd >= highRnd);
+
+                        for (int i = 0; i < numberOfInts; i++)
+                        {
+                            Random rnd = new Random();
+                            int num = rnd.Next(lowRnd, highRnd);
+                            IntList.Add(num);
+                        }
+
+                        // serializes the elements of IntList and writes them to fileName.json, with indentation
+                        string jsonWithRnd = JsonConvert.SerializeObject(IntList, Formatting.Indented);
+                        File.WriteAllText(fileName, jsonWithRnd);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Felaktig inmatning");
+                        break;
+                }
             }
         }
 
